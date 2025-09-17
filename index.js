6ca3cf1d2b620e753b259feb776347a35036d46e
Nofal Ali <nofilkachhot@gmail.com>
2025-09-17 12:48:33 +0530

    update routes in express.js

--- index.js
+++ index.js
@@ -1,19 +1,143 @@
-import http from "http";
+import express from 'express'
 
+const app = express()
 const port = 8000;
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.end("this is home route");
-  } else if (req.url === "/about") {
-    res.end("this is about route");
-  } else if (req.url === "/contact") {
-    res.end("this is contact route");
-  } else {
-    res.end("404 not found");
-  }
-});
+app.use(express.json())
 
-server.listen(8000, () => {
-  console.log("server is started");
-});
+// app.get("/", (req,res) => {
+//     res.json({name: "Nofal", age: "22"})
+// })
+// app.post("/", (req,res) => {
+//     const body = req.body
+//     console.log(body)
+//     res.send("hello post")
+// })
+// app.get("/contact", (req,res) => {
+//     res.send("contact page")
+// })
+
+app.listen(port, () => {
+    console.log("server is started")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import http from "http";
+
+// const port = 8000;
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     res.end("this is home route");
+//   } else if (req.url === "/about") {
+//     res.end("this is about route");
+//   } else if (req.url === "/contact") {
+//     res.end("this is contact route");
+//   } else {
+//     res.end("404 not found");
+//   }
+// });
+
+// server.listen(8000, () => {
+//   console.log("server is started");
+// });
