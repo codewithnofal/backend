8037e5fb53fb3f8e1b2395a6f20684c163a9f88d
Nofal Ali <nofilkachhot@gmail.com>
2025-09-18 16:30:38 +0530

    update

--- MongoDb/index.js
+++ MongoDb/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import User from "./models/user.model.js";
 
 const app = express();
 const prot = 8000;
+app.use(express.json());
+
 const mongoURI =
   "mongodb+srv://nofilkachhot_db_user:nofal123@cluster0.iusamoh.mongodb.net/TechVerce";
 
@@ -17,6 +20,39 @@ const connectDb = async () => {
 
 app.get("/", (req, res) => {
   res.send("hello mongodb");
+});
+
+app.post("/create", async (req, res) => {
+  try {
+    const { name, age, email, username } = req.body;
+    const newUser = await User.create({
+      name,
+      age,
+      email,
+      username,
+    });
+    res.status(201).json({ message: "User created" });
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+});
+
+app.get("/read", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(400).json({ message: "user not found" });
+  }
+});
+
+app.get("/read/:username", async (req, res) => {
+  try {
+    const users = await User.findOne({username: req.params.username});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(400).json({ message: "user not found" });
+  }
 });
 
 app.listen(prot, () => {
